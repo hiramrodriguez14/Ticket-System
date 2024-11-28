8470f998dc29346f5a7cbad501f737e928a57878
fernan-castro <fernando.castro1@upr.edu>
2024-11-27 23:34:20 -0400

    major changes app, cliente, estadio

--- Cliente.java
+++ Cliente.java
@@ -1,63 +1,40 @@
-import java.util.ArrayList;
-
+import java.util.Objects;
 
 public class Cliente {
-
-    // private stuff to use for Cliente
     private String name;
     private String email;
-    private String phoneNum;
+    private String phoneNumber;
 
-    // constructor
-    public  Cliente(String name, String email, String phoneNum) {
+    public Cliente(String name, String email, String phoneNumber) {
         this.name = name;
         this.email = email;
-        this.phoneNum = phoneNum;
+        this.phoneNumber = phoneNumber;
     }
 
-    // getters and setters
-    public String getName() {return name;}
-
-    public void setName(String name) {this.name = name;}
-
-    public String getEmail() {return email;}
-
-    public void setEmail(String email) {this.email = email;}
-
-    public String getPhoneNum() {return phoneNum;}
-
-    public void setPhoneNum(String phoneNum) {this.phoneNum = phoneNum;}
-
- 
-    public boolean equals(Cliente cliente) {
-       if((this.getEmail()==cliente.getEmail())&&(this.getName()==cliente.getName())&&(this.getPhoneNum()==cliente.getPhoneNum())){
-           return true;
+    public String getName() {
+        return name;
     }
-    return false;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Cliente cliente = (Cliente) obj;
+        return Objects.equals(name, cliente.name) &&
+               Objects.equals(email, cliente.email) &&
+               Objects.equals(phoneNumber, cliente.phoneNumber);
     }
 
     @Override
     public int hashCode() {
-        if(email!=null){
-            return email.hashCode(); //Returns the hashCode of the email string
-        }else{
-            return 0;
-        }
-    }
-    
-    // main method
-    public static void main(String[] args) {
-
-        
-        ArrayList<Cliente> f = new ArrayList<>();
-
-        Cliente fernando = new Cliente("Fernando", "fer.cas@upr.edu", "787");
-        f.add(fernando);
-        
-        for(Cliente c : f) {
-            System.out.println(c.getName());
-            System.out.println(c.getEmail());
-            System.out.println(c.getPhoneNum());
-        }
+        return Objects.hash(name, email, phoneNumber);
     }
 }
