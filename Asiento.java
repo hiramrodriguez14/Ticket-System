1ee916f25088a748ec886d365da026103c7525de
AkimPerez <akim.perez@upr.edu>
2024-11-27 22:53:52 -0400

    Documentation Cliente and Asiento

--- Asiento.java
+++ Asiento.java
@@ -1,50 +1,101 @@
+/**
+ * The Asiento class represents a seat in a stadium. 
+ * Each seat has a section, row, and seat number to uniquely identify it.
+ */
 public class Asiento {
+
+    // Private fields to store the section, row and seat number of seats.  
     private String section;
     private int row;
     private int seatNumber;
 
+
+    /**
+     * Constructs a new Asiento object with the specified section, row, and seat number.
+     *
+     * @param section    the section where the seat is located
+     * @param row        the row number of the seat
+     * @param seatNumber the seat number within the row
+     */
     public Asiento(String section, int row, int seatNumber) {
         this.section = section;
         this.row = row;
         this.seatNumber = seatNumber;
     }
 
+    /** 
+     * Returns the section where the seat is located. 
+     * @return the section as a     String 
+     */
     public String getSection() {
         return section;
     }
 
+    /**
+     * Returns the row number of seats. 
+     * @return the row number as an integer.  
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Returns the seat number withing the row. 
+     * @return the seat number as an integer.  
+     */
     public int getSeatNumber() {
         return seatNumber;
     }
 
+    /**
+     * Sets the section for the seat.
+     * @param section the section to set
+     */
     public void setSection(String section) {
         this.section = section; 
     }
 
+    /**
+     * Sets the row number for the seat.
+     * @param row the row number to set
+     */
     public void setRow(int row) {
         this.row = row; 
     }
 
+    /**
+     * Sets the seat number within the row.
+     * @param seatNumber the seat number to set
+     */
     public void setSeatNumber(int seatNumber) {
         this.seatNumber = seatNumber; 
     }
 
+    /**
+     * Returns a string representation of the seat.
+     * The format includes the section, row, and seat number.
+     * @return a string representing the seat with all its parts. 
+     */
     @Override
     public String toString() {
         return "Section: " + section + ", Row: " + row + ", SeatNumber: " + seatNumber;
     }
 
+    /**
+     * Compares this seat to another seat for equality.
+     * Two seats are considered equal if they have the same section, row, and seat number.
+     * @param asiento the seat to compare to
+     * @return true if the seats are equal, false otherwise
+     */
     public boolean equals(Asiento asiento) {
-        if((this.getSection() == asiento.getSection()) && (this.getRow() == asiento.getRow()) && (this.getSeatNumber() == asiento.getSeatNumber())) {
-            return true;
-        }
-        return false;
+        return (this.getSection().equals(asiento.getSection())) && (this.getRow() == asiento.getRow()) && (this.getSeatNumber() == asiento.getSeatNumber());
     }
 
+    /**
+     * Returns a hash code value for the seat.
+     * The hash code is based on the section, row, and seat number.
+     * @return the hash code of the seat
+     */
     @Override
     public int hashCode() {
         int result = 0; 
--- Cliente.java
+++ Cliente.java
@@ -1,63 +1,98 @@
-import java.util.ArrayList;
 
 
+/**
+ * The Cliente class represents a customer with personal details such as name, email, and phone number.
+ * It provides methods to access and modify these details, as well as to compare Cliente objects for equality purposes.
+ */
 public class Cliente {
 
-    // private stuff to use for Cliente
+    // Private fields to store customer details
     private String name;
     private String email;
     private String phoneNum;
 
-    // constructor
-    public  Cliente(String name, String email, String phoneNum) {
+    /**
+     * Constructs a new Cliente object with the specified name, email, and phone number.
+     *
+     * @param name     the name of the customer
+     * @param email    the email address of the customer
+     * @param phoneNum the phone number of the customer
+     */
+    public Cliente(String name, String email, String phoneNum) {
         this.name = name;
         this.email = email;
         this.phoneNum = phoneNum;
     }
 
-    // getters and setters
+    /**
+     * Returns the name of the customer.
+     *
+     * @return the name of the customer
+     */
     public String getName() {return name;}
 
+    /**
+     * Sets the name of the customer.
+     *
+     * @param name the name to set
+     */
     public void setName(String name) {this.name = name;}
 
+    /**
+     * Returns the email address of the customer.
+     *
+     * @return the email address of the customer
+     */
     public String getEmail() {return email;}
 
+    /**
+     * Sets the email address of the customer.
+     *
+     * @param email the email address to set
+     */
     public void setEmail(String email) {this.email = email;}
 
+    /**
+     * Returns the phone number of the customer.
+     *
+     * @return the phone number of the customer
+     */
     public String getPhoneNum() {return phoneNum;}
 
+    /**
+     * Sets the phone number of the customer.
+     *
+     * @param phoneNum the phone number to set
+     */
     public void setPhoneNum(String phoneNum) {this.phoneNum = phoneNum;}
 
  
+    /**
+     * Compares this Cliente object to another for equality.
+     * Two Cliente objects are considered equal if they have the same name, email, and phone number.
+     *
+     * @param cliente the Cliente object to compare to
+     * @return true if the Cliente objects are equal, false otherwise
+     */
     public boolean equals(Cliente cliente) {
-       if((this.getEmail()==cliente.getEmail())&&(this.getName()==cliente.getName())&&(this.getPhoneNum()==cliente.getPhoneNum())){
+        if((this.getEmail().equals(cliente.getEmail())) && (this.getName().equals(cliente.getName())) && (this.getPhoneNum().equals(cliente.getPhoneNum()))){
            return true;
-    }
-    return false;
+        }
+        return false;
     }
 
+    /**
+     * Returns the hash code for the Cliente object.
+     * The hash code is based on the email address of the customer.
+     *
+     * @return the hash code of the email, or 0 if the email is null
+     */
     @Override
     public int hashCode() {
         if(email!=null){
             return email.hashCode(); //Returns the hashCode of the email string
         }else{
             return 0;
-        }
-    }
-    
-    // main method
-    public static void main(String[] args) {
-
-        
-        ArrayList<Cliente> f = new ArrayList<>();
-
-        Cliente fernando = new Cliente("Fernando", "fer.cas@upr.edu", "787");
-        f.add(fernando);
-        
-        for(Cliente c : f) {
-            System.out.println(c.getName());
-            System.out.println(c.getEmail());
-            System.out.println(c.getPhoneNum());
         }
     }
 }
